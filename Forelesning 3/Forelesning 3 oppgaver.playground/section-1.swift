fe0e506ff21db8583efb4c3372f0451050015c66
Øyvind <oyvind.midtbo@bekk.no>
2015-02-25 17:02:20 +0100

    Gjort oppgave 2 forelesning 3

--- Forelesning 3/Forelesning 3 oppgaver.playground/section-1.swift
+++ Forelesning 3/Forelesning 3 oppgaver.playground/section-1.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 // OPPGAVE 1
 // Bruk matrise stuct'en under og lag et subscript for å hente ut og sette verdier i matrisen. Verdien du skal hente ut og sette er en Double.
 /*
@@ -50,3 +52,59 @@ matrix[0, 1] = 2
 matrix[1, 1] = 3
 
 matrix // prints [0.0, 2.0, 1.0, 3.0]
+
+// OPPGAVE 2
+// Svar på hvorfor kompilatoren feiler når du limer inn klassen uten kontruktør? Svar: Fordi variablene må initialiseres, de er nemlig ikke optionals eller har default verdi.
+// Lag en designated konstruktør og en convenience kontruktør for denne klassen
+// Lag nye instanser av klassen ved hjelp av de to kontruktørene du lagde i oppgave 2.2
+/*
+class Time {
+    var hour: Int
+    var minute: Int
+    var second: Int
+
+    var running: Bool = false
+    var startedAt: NSDate?
+    var stoppedAt: NSDate?
+}
+*/
+
+class Time {
+    var hour: Int
+    var minute: Int
+    var second: Int
+    
+    var running: Bool = false
+    var startedAt: NSDate?
+    var stoppedAt: NSDate?
+    
+    init(hour: Int, minute: Int, second: Int) {
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+    }
+    
+    convenience init() {
+        self.init(hour: 0, minute: 12, second: 30)
+    }
+}
+
+var time1 = Time(hour: 10, minute: 20, second: 30)
+var time2 = Time()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
